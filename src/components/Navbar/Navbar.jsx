0e3486cebce01671133368ea883781bb557c4993
Somya Guglani <somyaguglani0897@gmail.com>
2020-12-20 20:02:49 +0530

    navbar completed some changes to css may be required

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { GiDirectionSigns } from "react-icons/gi";
+import { FiMenu } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
+import css from "../Navbar/Navbar.module.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [clicked, changeClicked] = useState(false);
+
+  const handleClick = () => {
+    changeClicked((prevClicked) => !prevClicked);
+  };
+
+  const closeMobileMenu = () => {
+    changeClicked((prevClicked) => false);
+  };
+
+  return (
+    <>
+      <nav className={css.Navbar}>
+        <div className={css.NavbarContainer}>
+          <Link to="/" className={css.NavbarLogo}>
+            MOODPATH
+            <GiDirectionSigns className={css.MoodpathIcon} />
+          </Link>
+
+          <div className={css.MenuIcon} onClick={handleClick}>
+            {clicked ? (
+              <FiX className={css.MenuIcons} />
+            ) : (
+              <FiMenu className={css.MenuIcons} />
+            )}
+          </div>
+
+          <ul className={clicked ? css.NavMenuActive : css.NavMenu}>
+            <li className={css.NavItem}>
+              <Link to="/" className={css.NavLinks} onClick={closeMobileMenu}>
+                FAQ
+              </Link>
+            </li>
+            <li className={css.NavItem}>
+              <Link
+                to="/reviews"
+                className={css.NavLinks}
+                onClick={closeMobileMenu}
+              >
+                Reviews
+              </Link>
+            </li>
+            <li className={css.NavItem}>
+              <Link
+                to="/contact-us"
+                className={css.NavLinks}
+                onClick={closeMobileMenu}
+              >
+                Contact Us
+              </Link>
+            </li>
+            <li className={css.NavItem}>
+              <Link
+                to="/login"
+                className={css.NavLinks}
+                onClick={closeMobileMenu}
+              >
+                <button className={css.MenuButton}>Login</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
