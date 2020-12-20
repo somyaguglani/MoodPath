7c394b7ea0001ba5006285f5ea0bab3102920bd3
Somya Guglani <somyaguglani0897@gmail.com>
2020-12-21 01:34:58 +0530

    completed out mission section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Discover from "../Discover/Discover";
+import css from "../Home/Home.module.css";
 function Home() {
   return (
     <>
       <Discover />
+      <div className={css.MissionContainer}>
+        <h1>Our Mission</h1>
+        <p>
+          Making professional counseling accessible
+          <br />
+          and convenient - so anyone who struggles with life’s challenges can
+          get help, anytime, anywhere.
+        </p>
+      </div>
     </>
   );
 }
