00f94fe8eb9b004146015dc616dc798e7556181e
Somya Guglani <somyaguglani0897@gmail.com>
2020-12-24 05:51:17 +0530

    carousel is done just add the pictures and use helpers i made

---
+++ src/helpers/CarouselSettings.js
@@ -0,0 +1,14 @@
+const CarouselSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: true,
+  cssEase: "linear",
+};
+
+export default CarouselSettings;
